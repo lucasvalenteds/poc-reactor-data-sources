ce8b5ec511b06422ea7c4764464d124f94a10e86
Lucas <lucasvalenteds@gmail.com>
2020-11-13 19:14:10 -0300

    Adding Gradle build script

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+    java
+}
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    implementation("io.projectreactor", "reactor-core", properties["version.reactor"].toString())
+    testImplementation("io.projectreactor", "reactor-test", properties["version.reactor"].toString())
+    testImplementation("org.junit.jupiter", "junit-jupiter", properties["version.junit"].toString())
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    testLogging {
+        events = setOf(
+                TestLogEvent.PASSED,
+                TestLogEvent.FAILED,
+                TestLogEvent.SKIPPED
+        )
+    }
+}
